9ba6d5c22ed18fe6e453f54f4b35873f8b9a2161
zwformi <1156043710@qq.com>
2017-06-10 19:16:32 +0800

    【添加】spring_activeMQ的测试例子

---
+++ src/main/java/com/zw/springactivemq/mq/consumer/queue/QueueReceiver1.java
@@ -0,0 +1,28 @@
+
+package com.zw.springactivemq.mq.consumer.queue;
+
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * 
+ * @author liang
+ * @description  队列消息监听器
+ * 
+ */
+@Component("queueReceiver1")
+public class QueueReceiver1 implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+		try {
+			System.out.println("QueueReceiver1接收到消息:"+((TextMessage)message).getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/com/zw/springactivemq/mq/consumer/queue/QueueReceiver2.java
@@ -0,0 +1,29 @@
+
+package com.zw.springactivemq.mq.consumer.queue;
+
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * 
+ * @author liang
+ * @description  队列消息监听器
+ * 
+ */
+@Component("queueReceiver2")
+public class QueueReceiver2 implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+		try {
+			System.out.println("QueueReceiver2接收到消息:"+((TextMessage)message).getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
